7d5b1ac2ecafc2c12c02525d5bb230ab3ab7f235
ParthS <parthshah576@gmail.com>
2019-10-26 12:38:19 +0530

    Added html files
    Connected app and project and added html files

--- myProject/gradesApp/views.py
+++ myProject/gradesApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    insert={'text1':'My Grades', 'text2':'My grades are my grades, none of your grades'}
+    return render(request,'gradesApp/homepage.html',context=insert)
